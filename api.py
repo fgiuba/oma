b53ca876d7d46933b2bac3f7a6ccbc498b8f7d2a
fgiuba <federico.giuba@copangroup.com>
2018-11-16 14:52:34 +0100

    Add api template

---
+++ api.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from flask import Flask, jsonify
+from gevent.pywsgi import WSGIServer
+
+
+app = Flask('MyApp')
+server_bind = '0.0.0.0'
+server_port = 12345
+max_connections = 1000
+
+# SSL/TLS params (recommended, but optional)
+ssl_certificate = None
+ssl_key = None
+
+
+@app.route('/hello/<string:name>', methods=['GET'])
+def hello(name):
+    """
+    ************************
+    * Write your API here! *
+    ************************
+    """
+    return jsonify('hello {}!'.format(name))
+
+
+if __name__ == '__main__':
+    config = {
+        'listener': (server_bind, server_port),
+        'application': app,
+        'spawn': max_connections,
+    }
+    if ssl_certificate:
+        config['certifle'] = ssl_certificate
+    if ssl_key:
+        config['keyfile'] = ssl_key
+    http_server = WSGIServer(**config)
+    http_server.serve_forever()
